46c9fe8fbf5380974331663e33c6d54ada90cfa6
Son Le <sonlvs1507@gmail.com>
2024-06-17 13:27:57 +0700

    feat: content loader

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { MotionDiv } from './MotionDiv';
+import ContentLoader from 'react-content-loader';
 
 export interface AnimeProp {
    id: string;
@@ -17,16 +19,36 @@ interface Prop {
    index: number;
 }
 
-function AnimeCard({ anime }: Prop) {
+const variants = {
+   hidden: {
+      opacity: 0,
+   },
+   visible: {
+      opacity: 1,
+   },
+};
+
+function AnimeCard({ anime, index }: Prop) {
    return (
-      <div className='max-w-sm rounded relative w-full'>
+      <MotionDiv
+         className='max-w-sm rounded relative w-full'
+         initial='hidden'
+         animate='visible'
+         variants={variants}
+         transition={{ delay: index * 0.25, ease: 'easeInOut', duration: 0.5 }}
+         viewport={{ amount: 0 }}
+      >
          <div className='relative w-full h-[37vh]'>
-            <Image
-               src={`https://shikimori.one/${anime.image.original}`}
-               alt={anime.name}
-               fill
-               className='rounded-xl'
-            />
+            {anime.image.original ? (
+               <Image
+                  src={`https://shikimori.one${anime.image.original}`}
+                  alt={anime.name}
+                  fill
+                  className='rounded-xl'
+               />
+            ) : (
+               <ContentLoader />
+            )}
          </div>
          <div className='py-4 flex flex-col gap-3'>
             <div className='flex justify-between items-center gap-1'>
@@ -60,7 +82,7 @@ function AnimeCard({ anime }: Prop) {
                </div>
             </div>
          </div>
-      </div>
+      </MotionDiv>
    );
 }
 
